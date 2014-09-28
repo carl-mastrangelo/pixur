6238fd2dde237016d09bc02e83fa1181ace9fe07
Carl Mastrangelo <carl@carlmastrangelo.com>
2014-09-28 03:27:15 +0000

    Enabled http access and a helloWorld template

--- pixur.go
+++ pixur.go
@@ -1,31 +1,58 @@
 package pixur
 
 import (
-  _ "log"
+ "log"
 
-  "database/sql"
-  "net/http"
-  _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+  "html/template"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-
 type Config struct {
-  MysqlConfig string `json:"mysql_config"`
-  HttpSpec string `json:"spec"`
+	MysqlConfig string `json:"mysql_config"`
+	HttpSpec    string `json:"spec"`
 }
 
 type Server struct {
-  db *sql.DB 
+	db *sql.DB
+  s *http.Server
 }
 
-func (s *Server) StartAndWait(c *Config) error {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  tpl, err := template.ParseFiles("tpl/index.html")
+  if err != nil {
+    log.Println(err)
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  if err := tpl.Execute(w, nil); err != nil {
+    log.Println(err)
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+}
+
+func (s *Server) setup(c *Config) error {
 	db, err := sql.Open("mysql", c.MysqlConfig)
 	if err != nil {
 		return err
 	}
-  s.db = db
+	if err := db.Ping(); err != nil {
+		return err
+	}
+	s.db = db
   
-  
-  return http.ListenAndServe(c.HttpSpec, nil)
+  s.s = new(http.Server)
+  s.s.Addr = c.HttpSpec
+  mux := http.NewServeMux()
+  s.s.Handler = mux
+  mux.HandleFunc("/", indexHandler)
+  return nil
 }
 
+func (s *Server) StartAndWait(c *Config) error {
+	s.setup(c)
+	return s.s.ListenAndServe()
+}
--- starter/main.go
+++ starter/main.go
@@ -1,55 +1,53 @@
-package main 
+package main
 
 import (
-  "log"
-  "flag"
-  "os"
-  "encoding/json"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
 
-  "pixur.org/pixur"
+	"pixur.org/pixur"
 )
 
 var (
-  config = flag.String("config", ".config.json", "The default configuration file")
-  mysqlConfig = flag.String("mysql_config", "", "The default mysql config")
-  spec = flag.String("spec", ":8888", "Default HTTP port")
+	config      = flag.String("config", ".config.json", "The default configuration file")
+	mysqlConfig = flag.String("mysql_config", "", "The default mysql config")
+	spec        = flag.String("spec", ":8888", "Default HTTP port")
 )
 
-
 func getConfig(path string) (*pixur.Config, error) {
-  var config = new(pixur.Config)
-  f, err := os.Open(path)
+	var config = new(pixur.Config)
+	f, err := os.Open(path)
 
-  if os.IsNotExist(err) {
-    log.Println("Unable to open config file, using defaults")
-    return config, nil
-  } else if err != nil {
-    return nil, err
-  }
-  defer f.Close()
-  
-  configDecoder := json.NewDecoder(f)
-  if err := configDecoder.Decode(config); err != nil {
-    return nil, err
-  }
-  
-  return config, nil
+	if os.IsNotExist(err) {
+		log.Println("Unable to open config file, using defaults")
+		return config, nil
+	} else if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	configDecoder := json.NewDecoder(f)
+	if err := configDecoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
 
-
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  c, err := getConfig(*config)
-  if err != nil {
-    log.Fatal(err)
-  }
-  if *mysqlConfig != "" {
-    c.MysqlConfig = *mysqlConfig
-  }
-  c.HttpSpec = *spec
-  
-  s := &pixur.Server{}
-  
-  log.Fatal(s.StartAndWait(c))
+	c, err := getConfig(*config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *mysqlConfig != "" {
+		c.MysqlConfig = *mysqlConfig
+	}
+	c.HttpSpec = *spec
+
+	s := &pixur.Server{}
+
+	log.Fatal(s.StartAndWait(c))
 }
