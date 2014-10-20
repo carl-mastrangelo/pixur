e1370a9bc1d6f1ea92a6b4dee510b357c959a8b4
Carl Mastrangelo <carl@carlmastrangelo.com>
2014-10-20 06:42:10 +0000

    Use case sensitive columns for tag names

--- schema.go
+++ schema.go
@@ -19,22 +19,22 @@ var (
 
 		"CREATE TABLE IF NOT EXISTS `tags` (" +
 			"  `id` int(11) NOT NULL AUTO_INCREMENT," +
-			"  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL," +
+			"  `name` varchar(255) COLLATE utf8_bin NOT NULL," +
 			"  `created_time` bigint(20) NOT NULL," +
 			"  `modified_time` bigint(20) NOT NULL," +
 			"  PRIMARY KEY (`id`)," +
 			"  UNIQUE KEY `name` (`name`)" +
-			"  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;",
+			"  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;",
 
 		"CREATE TABLE IF NOT EXISTS `pictags` (" +
 			"  `pic_id` int(11) NOT NULL," +
 			"  `tag_id` int(11) NOT NULL," +
-			"  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL," +
+			"  `name` varchar(255) COLLATE utf8_bin NOT NULL," +
 			"  `created_time` bigint(20) NOT NULL," +
 			"  `modified_time` bigint(20) NOT NULL," +
 			"  PRIMARY KEY (`pic_id`,`tag_id`)," +
 			"  KEY `tag_id` (`tag_id`)" +
-			"  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;",
+			"  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;",
 	}
 )
 
