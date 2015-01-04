27dfda36e2e224601f3aaf9e76206fcd254f37cd
Carl Mastrangelo <carl@carlmastrangelo.com>
2015-01-04 07:22:25 +0000

    Use Angular js routes.  This makes the back button work, and links actually go somewhere.

--- static/pixur.js
+++ static/pixur.js
@@ -1,59 +1,61 @@
 (function(){
-var IndexCtrl = function($scope, $location, indexPicsService, createPicService) {
-	this.indexPicsService_ = indexPicsService;
-	this.createPicService_ = createPicService;
-	this.location_ = $location;
-
+var IndexCtrl = function($scope, $location, $routeParams, indexPicsService, createPicService) {
+  this.indexPicsService_ = indexPicsService;
+  this.createPicService_ = createPicService;
+  this.location_ = $location;
+  console.log("loaded again");
   this.pics = [];
-	
-	this.pageIDIndex = -1;
-	this.pageIDs = [];
 
-	this.nextPageID = null;
-	this.prevPageID = null;
+  this.nextPageID = null;
+  this.prevPageID = null;
 
   this.upload = {
     file: null, 
     url: "",
   };
+  var startId = 0;
+  if ($routeParams.picId) {
+    startId = $routeParams.picId;
+  }
+  console.log(startId);
 
-	// Initial Load
-  indexPicsService.get().then(
+  // Initial Load
+  indexPicsService.get(startId).then(
     function(data) {
-			var pics = data.data;
-			if (pics.length > 0) {
-				this.nextPageID = pics[pics.length - 1].id;
-				this.pics = pics;
-			}
+      var pics = data.data;
+      if (pics.length > 0) {
+        this.nextPageID = pics[pics.length - 1].id;
+        this.pics = pics;
+      }
     }.bind(this)
   );
 }
 
 IndexCtrl.prototype.loadNext = function() {
-	this.indexPicsService_.get(this.nextPageID).then(
-		function(data) {
-			this.pics = data.data;
-			if (this.pics.length > 0) {
-				this.nextPageID = this.pics[this.pics.length - 1].id
-			}
-		}.bind(this)
-	);
+  this.indexPicsService_.get(this.nextPageID).then(
+    function(data) {
+      this.pics = data.data;
+      if (this.pics.length > 0) {
+        this.nextPageID = this.pics[this.pics.length - 1].id
+      }
+    }.bind(this)
+  );
 
 }
 
 IndexCtrl.prototype.fileChange = function(elem) {
-	if (elem.files.length > 0) {
-		this.upload.file = elem.files[0];
-	} else {
-		this.upload.file = null;
-	}
+  if (elem.files.length > 0) {
+    this.upload.file = elem.files[0];
+  } else {
+    this.upload.file = null;
+  }
 };
 
 IndexCtrl.prototype.createPic = function() {
-	this.createPicService_.create(this.upload.file, this.upload.url)
-			.then(function(data) {
-				this.pics.unshift(data.data);
-			}.bind(this));
+  this.createPicService_.create(this.upload.file, this.upload.url)
+      .then(function(data) {
+        this.pics.unshift(data.data);
+      }.bind(this));
 };
 
 var IndexPicsService = function($http, $q) {
@@ -63,12 +65,12 @@ var IndexPicsService = function($http, $q) {
 
 IndexPicsService.prototype.get = function(startID) {
   var deferred = this.q_.defer();
-	var httpConfig = {};
-	if (startID) {
-		httpConfig.params = {
-			start_pic_id: startID
-		};
-	}
+  var httpConfig = {};
+  if (startID) {
+    httpConfig.params = {
+      start_pic_id: startID
+    };
+  }
   this.http_.get("/api/findIndexPics", httpConfig).then(
     function(data, status, headers, config) {
       deferred.resolve(data);
@@ -111,19 +113,35 @@ CreatePicService.prototype.create = function(file, url) {
 };
 
 var ScrollDirective = function($window) {
-	return function(scope, element, attrs) {
-		angular.element($window).bind("scroll", function() {
-			//console.log(this);
-		});
-	};
+  return function(scope, element, attrs) {
+    angular.element($window).bind("scroll", function() {
+      //console.log(this);
+    });
+  };
 };
 
-angular.module('pixur', [])
-		.config(function($locationProvider) {
-			$locationProvider.html5Mode(true);
-		})
+angular.module('pixur', [
+      "ngRoute"
+    ])
+    .config(function($locationProvider) {
+      $locationProvider.html5Mode(true);
+    })
+    .config(function($routeProvider) {
+      $routeProvider
+          .when("/", {
+            templateUrl: "static/index.html",
+            controller: "IndexCtrl",
+            controllerAs: "ctrl"
+          })
+          .when("/i/:picId?", {
+            templateUrl: "static/index.html",
+            controller: "IndexCtrl",
+            controllerAs: "ctrl"
+          })
+      ;
+    })
     .controller("IndexCtrl", IndexCtrl)
     .service("indexPicsService", IndexPicsService)
     .service("createPicService", CreatePicService)
-		.directive("scroll", ScrollDirective);
+    .directive("scroll", ScrollDirective);
 })();
