a2a76b625c9e973f4261c747f1a758b1ec20a589
Carl Mastrangelo <carl@carlmastrangelo.com>
2015-09-27 16:26:25 -0700

    Add batch job support and cron pic deleter

---
+++ tools/batch/batch.go
@@ -0,0 +1,109 @@
+package batch
+
+import (
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
+	"pixur.org/pixur"
+	"pixur.org/pixur/schema"
+)
+
+// copy of starter/
+var (
+	config = flag.String("config", ".config.json", "The default configuration file")
+)
+
+type ServerConfig struct {
+	DB      *sql.DB
+	PixPath string
+}
+
+var serverConfig *ServerConfig
+
+func GetConfig() (*ServerConfig, error) {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+	if serverConfig != nil {
+		return serverConfig, nil
+	}
+
+	c, err := getConfig(*config)
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open("mysql", c.MysqlConfig)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(20)
+
+	sc := &ServerConfig{
+		DB:      db,
+		PixPath: c.PixPath,
+	}
+
+	serverConfig = sc
+
+	return sc, nil
+}
+
+func ForEachPic(fn func(*schema.Pic, *ServerConfig, error) error) error {
+	sc, err := GetConfig()
+	if err != nil {
+		return err
+	}
+	stmt, err := schema.PicPrepare("SELECT * FROM_ WHERE %s > ? ORDER BY %s LIMIT 1000;",
+		sc.DB, schema.PicColId, schema.PicColId)
+	if err != nil {
+		return err
+	}
+
+	var i int64
+	var fnErr error
+	for {
+		pics, err := schema.FindPics(stmt, i)
+		if err != nil {
+			return err
+		}
+		if len(pics) == 0 {
+			break
+		}
+
+		for _, p := range pics {
+			fnErr = fn(p, sc, fnErr)
+			i = p.PicId
+		}
+	}
+	if fnErr != nil {
+		return fnErr
+	}
+	return nil
+}
+
+func getConfig(path string) (*pixur.Config, error) {
+	var config = new(pixur.Config)
+	f, err := os.Open(path)
+
+	if os.IsNotExist(err) {
+		log.Println("Unable to open config file, using defaults")
+		return config, nil
+	} else if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	configDecoder := json.NewDecoder(f)
+	if err := configDecoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
---
+++ tools/cron/deleted_file_pruner.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"pixur.org/pixur/schema"
+	"pixur.org/pixur/tasks"
+	"pixur.org/pixur/tools/batch"
+)
+
+func main() {
+	err := batch.ForEachPic(func(p *schema.Pic, sc *batch.ServerConfig, err error) error {
+		if err != nil {
+			return err
+		}
+		now := time.Now()
+		// No deletion info
+		if p.DeletionStatus == nil {
+			return nil
+		}
+		// Some deletion info, but it isn't on the chopping block.
+		if p.DeletionStatus.PendingDeletedTs == nil {
+			return nil
+		}
+		// It was already hard deleted, ignore it
+		if p.DeletionStatus.ActualDeletedTs != nil {
+			return nil
+		}
+
+		pendingTime := schema.ToTime(p.DeletionStatus.PendingDeletedTs)
+		// It is pending deletion, just not yet.
+		if !now.After(pendingTime) {
+			return nil
+		}
+
+		log.Println("Preparing to delete", p.GetVarPicID(), pendingTime)
+		log.Printf("%+v", p)
+		var task = &tasks.HardDeletePicTask{
+			DB:      sc.DB,
+			PixPath: sc.PixPath,
+			PicID:   p.PicId,
+		}
+		runner := new(tasks.TaskRunner)
+		if err := runner.Run(task); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+}
