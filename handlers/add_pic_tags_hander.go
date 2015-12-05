ca23e68c95d606f30155af4b9c6b2c26eaffe386
Carl Mastrangelo <carl@carlmastrangelo.com>
2015-12-04 20:27:29 -0800

    Add endpoint for adding tags

---
+++ handlers/add_pic_tags_hander.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"database/sql"
+	"net/http"
+
+	"pixur.org/pixur/schema"
+	"pixur.org/pixur/tasks"
+)
+
+type AddPicTagsHandler struct {
+	// embeds
+	http.Handler
+
+	// deps
+	DB *sql.DB
+}
+
+// TODO: add tests
+func (h *AddPicTagsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Unsupported Method", http.StatusMethodNotAllowed)
+		return
+	}
+	var requestedPicID int64
+	if raw := r.FormValue("pic_id"); raw != "" {
+		var vid schema.Varint
+		if err := vid.DecodeAll(raw); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		} else {
+			requestedPicID = int64(vid)
+		}
+	}
+
+	var task = &tasks.AddPicTagsTask{
+		DB: h.DB,
+
+		PicID:    requestedPicID,
+		TagNames: r.PostForm["tag"],
+	}
+	runner := new(tasks.TaskRunner)
+	if err := runner.Run(task); err != nil {
+		returnTaskError(w, err)
+		return
+	}
+
+	returnJSON(w, r, true)
+}
+
+func init() {
+	register(func(mux *http.ServeMux, c *ServerConfig) {
+		mux.Handle("/api/addPicTags", &AddPicTagsHandler{
+			DB: c.DB,
+		})
+	})
+}
---
+++ tasks/add_pic_tags_task.go
@@ -0,0 +1,49 @@
+package tasks
+
+import (
+	"database/sql"
+	"time"
+
+	"pixur.org/pixur/schema"
+	s "pixur.org/pixur/status"
+)
+
+type AddPicTagsTask struct {
+	// Deps
+	DB  *sql.DB
+	Now func() time.Time
+
+	// Inputs
+	PicID    int64
+	TagNames []string
+}
+
+// TODO: add tests
+func (t *AddPicTagsTask) Run() error {
+	tx, err := t.DB.Begin()
+	if err != nil {
+		return s.InternalError(err, "Can't Begin tx")
+	}
+	defer tx.Rollback()
+
+	stmt, err := schema.PicPrepare("SELECT * FROM_ WHERE %s = ? LOCK IN SHARE MODE;",
+		tx, schema.PicColId)
+	if err != nil {
+		return s.InternalError(err, "Can't prepare stmt")
+	}
+	defer stmt.Close()
+
+	p, err := schema.LookupPic(stmt, t.PicID)
+	if err == sql.ErrNoRows {
+		return s.NotFound(err, "Can't find pic")
+	}
+
+	if err := upsertTags(tx, t.TagNames, p.PicId, t.Now()); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return s.InternalError(err, "Can't Commit tx")
+	}
+	return nil
+}
