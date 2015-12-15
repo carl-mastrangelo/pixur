b56bbf33373af0fe3c7174b4f72cedecea30cb03
Carl Mastrangelo <carl@carlmastrangelo.com>
2015-12-15 09:53:34 -0800

    Add workflow test for user creation

--- tasks/create_user_task_test.go
+++ tasks/create_user_task_test.go
@@ -1,10 +1,52 @@
 package tasks
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/binary"
 	"testing"
+	"time"
 
+	"github.com/golang/protobuf/proto"
+
+	"pixur.org/pixur/schema"
 	s "pixur.org/pixur/status"
 )
+
+func TestCreateUserWorkFlow(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+	now := time.Now()
+
+	task := &CreateUserTask{
+		DB:     c.DB(),
+		Now:    func() time.Time { return now },
+		Email:  "email",
+		Secret: "secret",
+	}
+
+	if err := task.Run(); err != nil {
+		t.Fatal(err)
+	}
+
+	key := make([]byte, 8)
+	binary.BigEndian.PutUint64(key, 1)
+	mac := hmac.New(sha256.New, key)
+	mac.Write([]byte("secret"))
+
+	expected := &schema.User{
+		UserId:     1,
+		Secret:     mac.Sum(nil),
+		CreatedTs:  schema.ToTs(now),
+		ModifiedTs: schema.ToTs(now),
+		Ident: []*schema.UserIdent{{
+			Email: "email",
+		}},
+	}
+	if !proto.Equal(expected, task.CreatedUser) {
+		t.Fatal("not equal", expected, task.CreatedUser)
+	}
+}
 
 func TestCreateUserEmptyEmail(t *testing.T) {
 	c := Container(t)
