0aed0d9f27c56495d01907eaca9092458309aaf7
Carl Mastrangelo <carl@carlmastrangelo.com>
2016-04-04 10:31:22 -0700

    Fix bug in Dav handler

--- handlers/dav_handler.go
+++ handlers/dav_handler.go
@@ -2,17 +2,14 @@ package handlers
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 
 	"golang.org/x/net/webdav"
 
 	"pixur.org/pixur/schema"
-	_ "pixur.org/pixur/tasks"
 )
 
 type PixFS struct {
@@ -36,33 +33,16 @@ func (fs *PixFS) Rename(oldName, newName string) error {
 }
 
 func (fs *PixFS) Stat(name string) (os.FileInfo, error) {
-	if len(filepath.Ext(name)) != 0 {
-		base := strings.TrimSuffix(filepath.Base(name), filepath.Ext(name))
-		fmt.Println(name, "  ", base)
-		var v schema.Varint
-		if consumed, err := v.Decode(base); err != nil {
-			return nil, os.ErrNotExist
-		} else if len(base) > consumed {
-			// Thumbnails have this property
-			return nil, os.ErrNotExist
-		}
+	if isThumbnail(name) {
+		return nil, os.ErrNotExist
 	}
 
 	return fs.FileSystem.Stat(name)
 }
 
 func (fs *PixFS) OpenFile(name string, flag int, perm os.FileMode) (webdav.File, error) {
-	if len(filepath.Ext(name)) != 0 {
-		fmt.Println(name + "zzz")
-		// Exclude thumbnails from showing up, which are noisy.
-		base := strings.TrimSuffix(filepath.Base(name), filepath.Ext(name))
-		var v schema.Varint
-		if consumed, err := v.Decode(base); err != nil {
-			return nil, os.ErrNotExist
-		} else if len(base) > consumed {
-			// Thumbnails have this property
-			return nil, os.ErrNotExist
-		}
+	if isThumbnail(name) {
+		return nil, os.ErrNotExist
 	}
 	if flag != os.O_RDONLY || perm != 0 {
 		return nil, os.ErrPermission
@@ -88,7 +68,7 @@ func (w *wrapper) Readdir(count int) ([]os.FileInfo, error) {
 	var goodInfos []os.FileInfo
 
 	for _, info := range infos {
-		if strings.ContainsRune(path.Base(info.Name()), 'u') {
+		if isThumbnail(info.Name()) {
 			continue
 		}
 
@@ -96,6 +76,16 @@ func (w *wrapper) Readdir(count int) ([]os.FileInfo, error) {
 	}
 
 	return goodInfos, nil
+}
+
+func isThumbnail(name string) bool {
+	base := strings.TrimSuffix(filepath.Base(name), filepath.Ext(name))
+	var v schema.Varint
+	consumed, err := v.Decode(base)
+	if err != nil {
+		return false
+	}
+	return len(base) > consumed
 }
 
 func init() {
