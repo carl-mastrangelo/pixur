e4c737ca415f7174ab49b3290e8137b99ccfc54a
Carl Mastrangelo <carl@carlmastrangelo.com>
2016-04-24 11:27:38 -0700

    Vendor in plugin.proto

--- schema/db/vendor/descgen.go
+++ schema/db/vendor/descgen.go
@@ -3,7 +3,10 @@ package deskgen
 
 /*
 DESCRIPTOR_PATH="$(dirname `which protoc` | sed 's/bin/include/')/google/protobuf/descriptor.proto"
+PLUGIN_PATH="$(dirname `which protoc` | sed 's/bin/include/')/google/protobuf/compiler/plugin.proto"
 protoc $DESCRIPTOR_PATH --go_out=. &&
+protoc $PLUGIN_PATH --go_out=. &&
+
 
 exit 0
 */
