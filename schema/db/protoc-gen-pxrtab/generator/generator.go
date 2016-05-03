a08c6e09ddc67c72db562a0cdda42a723953ed21
Carl Mastrangelo <carl@carlmastrangelo.com>
2016-05-02 23:57:04 -0700

    Make indexes output correctly

--- schema/db/protoc-gen-pxrtab/generator/generator.go
+++ schema/db/protoc-gen-pxrtab/generator/generator.go
@@ -1,19 +1,76 @@
 package generator
 
 import (
-	_ "bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
-	_ "log"
+	"path/filepath"
+	"sort"
 	"strings"
-	_ "text/template"
 
 	"github.com/golang/protobuf/proto"
 	descriptor "google/protobuf"
 	plugin "google/protobuf/compiler"
 
 	"pixur.org/pixur/schema/db/model"
+)
+
+type file struct {
+	name    string
+	pack    string
+	imports deps
+	packmap map[string]string
+	tables  []table
+}
+
+type table struct {
+	name       string
+	gotype     string
+	datagotype string
+	columns    []*column
+	indexes    []index
+	msg        *descriptor.DescriptorProto
+}
+
+type coltype struct {
+	sqltype string
+	gotype  string
+}
+
+type column struct {
+	name   string
+	coltyp coltype
+	field  *descriptor.FieldDescriptorProto
+}
+
+type index struct {
+	name    string
+	keyType keyType
+	columns []*column
+}
+
+type deps []string
+
+func (d deps) Less(i, k int) bool {
+	left := strings.Split(d[i], " ")
+	right := strings.Split(d[k], " ")
+	return strings.Compare(left[1], right[1]) < 0
+}
+
+func (d deps) Len() int {
+	return len(d)
+}
+
+func (d deps) Swap(i, k int) {
+	d[k], d[i] = d[i], d[k]
+}
+
+type keyType string
+
+var (
+	primaryKey keyType = "PRIMARY KEY"
+	uniqueKey  keyType = "UNIQUE"
+	indexKey   keyType = "INDEX"
 )
 
 type Generator struct {
@@ -79,11 +136,19 @@ func (g *Generator) run(req *plugin.CodeGeneratorRequest) *plugin.CodeGeneratorR
 }
 
 func (g *Generator) generateFile(fds []*descriptor.FileDescriptorProto) (string, error) {
-	// plugin.proto says files are ordered by dependency, and we only allow one source file,
-	// so it must be last.
-	for _, fd := range fds {
+	var f file
+	f.packmap = make(map[string]string)
+	nameToDesc := make(map[string]*descriptor.FileDescriptorProto)
+	for i, fd := range fds {
 		if fd.GetOptions().GetGoPackage() == "" {
 			return "", fmt.Errorf("%s doesn't have go_package", *fd.Name)
+		}
+		nameToDesc[*fd.Name] = fd
+		if i == len(fds)-1 {
+			// Make ourself the empty package
+			f.packmap[*fd.Package] = ""
+		} else {
+			f.packmap[*fd.Package] = fd.GetOptions().GetGoPackage()
 		}
 
 		for _, msg := range fd.MessageType {
@@ -94,9 +159,381 @@ func (g *Generator) generateFile(fds []*descriptor.FileDescriptorProto) (string,
 			if err != nil {
 				return "", err
 			}
+			t, err := buildTable(msg, opts.(*model.TableOptions), f.packmap)
+			if err != nil {
+				return "", err
+			}
 
+			f.tables = append(f.tables, t)
 		}
 	}
 
-	return "", nil
+	// plugin.proto says files are ordered by dependency, and we only allow one source file,
+	// so it must be last.
+
+	f.pack = fds[len(fds)-1].GetOptions().GetGoPackage()
+	for _, dep := range fds[len(fds)-1].Dependency {
+		fd, ok := nameToDesc[dep]
+		if !ok {
+			panic("Missing file " + dep)
+		}
+		imp := fmt.Sprintf(`%s "%s"`,
+			filepath.Base(fd.GetOptions().GetGoPackage()), filepath.Dir(dep))
+		f.imports = append(f.imports, imp)
+	}
+
+	return renderTables(f), nil
+}
+
+func renderTables(f file) string {
+	w := &indentWriter{}
+	renderPackage(w, f)
+	w.writeln("")
+	renderImports(w, f)
+	w.writeln("")
+	renderSqlTables(w, f)
+	w.writeln("")
+	renderIndexes(w, f)
+	w.writeln("")
+	renderDefaultTypes(w, f)
+	w.writeln("")
+	renderScanFuncs(w, f)
+	renderFindFuncs(w, f)
+
+	return w.String()
+}
+
+func renderDefaultTypes(w *indentWriter, f file) {
+	w.writeln("type Job struct {")
+	w.in()
+	w.writeln("Tx *sql.Tx")
+	w.out()
+	w.writeln("}")
+}
+
+func renderScanFuncs(w *indentWriter, f file) {
+	for _, t := range f.tables {
+		w.writefln(`func (j Job) Scan%s(opts db.Opts, cb func(%s) error) error {`, t.name, t.datagotype)
+		w.in()
+		w.writefln(`return db.Scan(j.Tx, "%s", opts, func(data []byte) error {`, t.name)
+		w.in()
+		w.writefln(`var pb %s`, t.datagotype)
+		w.writeln("if err := proto.Unmarshal(data, &pb); err != nil {")
+		w.in()
+		w.writeln("return err")
+		w.out()
+		w.writeln("}")
+		w.writeln("return cb(pb)")
+		w.out()
+		w.writeln("})")
+		w.out()
+		w.writeln("}")
+		w.writeln("")
+	}
+}
+
+func renderFindFuncs(w *indentWriter, f file) {
+	for _, t := range f.tables {
+		w.writefln(`func (j Job) Find%s(opts db.Opts) (rows []%s, err error) {`, t.name, t.datagotype)
+		w.in()
+		w.writefln(`err = j.Scan%s(opts, func(data %s) error {`, t.name, t.datagotype)
+		w.in()
+		w.writeln(`rows = append(rows, data)`)
+		w.writeln(`return nil`)
+		w.out()
+		w.writeln(`})`)
+		w.writeln(`return`)
+		w.out()
+		w.writeln("}")
+		w.writeln("")
+	}
+}
+
+func renderPackage(w *indentWriter, f file) {
+	w.writeln("package " + f.pack)
+}
+
+func renderIndexes(w *indentWriter, f file) {
+	for _, t := range f.tables {
+		for _, idx := range t.indexes {
+			w.writefln(`var _ db.Idx = %s{}`, idx.name)
+			w.writefln(`type %s struct {`, idx.name)
+			w.in()
+			for _, c := range idx.columns {
+				w.writefln(`%s *%s`, colNameToGoName(c.name), c.coltyp.gotype)
+			}
+			w.out()
+			w.writeln("}")
+			w.writeln("")
+			w.writefln(`func (idx %s) Cols() []string {`, idx.name)
+			var escaped []string
+			for _, c := range idx.columns {
+				escaped = append(escaped, `"`+c.name+`"`)
+			}
+			w.in()
+			w.writefln(`return []string{%s}`, strings.Join(escaped, ", "))
+			w.out()
+			w.writeln("}")
+			w.writeln("")
+			w.writefln(`func (idx %s) Vals() (vals []interface{}) {`, idx.name)
+			w.in()
+			w.writeln("var done bool")
+			for _, c := range idx.columns {
+				w.writefln("if idx.%s != nil {", colNameToGoName(c.name))
+				w.in()
+				w.writeln("if done {")
+				w.in()
+				w.writefln(`panic("Extra value %s")`, colNameToGoName(c.name))
+				w.out()
+				w.writeln("}")
+				w.writefln("vals = append(vals, *idx.%s)", colNameToGoName(c.name))
+				w.out()
+				w.writeln("} else {")
+				w.in()
+				w.writeln("done = true")
+				w.out()
+				w.writeln("}")
+			}
+			w.writeln("return")
+			w.out()
+			w.writeln("}")
+			w.writeln("")
+		}
+	}
+}
+
+/*
+  var done bool
+  if idx.PicId != nil {
+    if done {
+      panic("extra value PicId")
+    }
+    vals = append(vals, *idx.PicId)
+  } else {
+    done = true
+  }
+
+*/
+
+func colNameToGoName(name string) string {
+	parts := strings.Split(name, "_")
+	for i := 0; i < len(parts); i++ {
+		parts[i] = strings.Title(parts[i])
+	}
+	return strings.Join(parts, "")
+}
+
+func renderImports(w *indentWriter, f file) {
+	sort.Sort(deps(f.imports))
+	w.writeln("import (")
+	w.in()
+	w.writeln(`"database/sql"`)
+	w.out()
+	w.writeln("")
+	w.in()
+	w.writeln(`"github.com/golang/protobuf/proto"`)
+	w.out()
+	w.writeln("")
+	w.in()
+	w.writeln(`"pixur.org/pixur/schema/db"`)
+	w.out()
+	w.writeln("")
+	w.in()
+	for _, imp := range f.imports {
+		w.writeln(imp)
+	}
+	w.out()
+	w.writeln(")")
+}
+
+func renderSqlTables(w *indentWriter, f file) {
+	w.writeln("var SqlTables = []string{")
+	w.in()
+	for _, t := range f.tables {
+		w.writefln(`"CREATE TABLE \"%s\" (" +`, t.name)
+		w.in()
+		for _, col := range t.columns {
+			w.writefln(`"\"%s\" %s NOT NULL, " +`, col.name, col.coltyp.sqltype)
+		}
+		var inlineIndexes []index
+		var indexes []index
+		for _, idx := range t.indexes {
+			if idx.keyType == indexKey {
+				indexes = append(indexes, idx)
+			} else {
+				inlineIndexes = append(inlineIndexes, idx)
+			}
+		}
+
+		for i, idx := range inlineIndexes {
+			switch idx.keyType {
+			case indexKey:
+				continue
+			}
+			var cols []string
+			for _, col := range idx.columns {
+				cols = append(cols, fmt.Sprintf(`\"%s\"`, col.name))
+			}
+			last := ", "
+			if i == len(inlineIndexes)-1 {
+				last = ""
+			}
+			w.writefln(`"%s(%s)%s" +`, idx.keyType, strings.Join(cols, ", "), last)
+		}
+		w.writeln(`");",`)
+		w.out()
+		for _, idx := range indexes {
+			var cols []string
+			for _, col := range idx.columns {
+				cols = append(cols, fmt.Sprintf(`\"%s\"`, col.name))
+			}
+			w.writefln(`"CREATE INDEX \"%s\" ON \"%s\" (%s);",`,
+				idx.name, t.name, strings.Join(cols, ", "))
+		}
+	}
+
+	w.out()
+	w.writeln("}")
+}
+
+func buildTable(msg *descriptor.DescriptorProto, opts *model.TableOptions,
+	packmap map[string]string) (table, error) {
+	t := table{}
+	if opts.Name != "" {
+		t.name = opts.Name
+	} else {
+		t.name = *msg.Name
+	}
+	t.gotype = *msg.Name
+	if strings.ContainsAny(t.name, "\"\\") {
+		return t, fmt.Errorf("Invalid characters in table name %s", t.name)
+	}
+	fieldNames := make(map[string]*column, len(msg.Field))
+	for _, f := range msg.Field {
+		var coltyp coltype
+		switch *f.Type {
+		case descriptor.FieldDescriptorProto_TYPE_FIXED32:
+			fallthrough
+		case descriptor.FieldDescriptorProto_TYPE_SFIXED32:
+			fallthrough
+		case descriptor.FieldDescriptorProto_TYPE_SINT32:
+			fallthrough
+		case descriptor.FieldDescriptorProto_TYPE_ENUM:
+			coltyp = coltype{
+				sqltype: "integer",
+				gotype:  typeNameToGoName(*f.TypeName, packmap),
+			}
+		case descriptor.FieldDescriptorProto_TYPE_INT32:
+			coltyp = coltype{
+				sqltype: "integer",
+				gotype:  "int32",
+			}
+		case descriptor.FieldDescriptorProto_TYPE_FIXED64:
+			fallthrough
+		case descriptor.FieldDescriptorProto_TYPE_SFIXED64:
+			fallthrough
+		case descriptor.FieldDescriptorProto_TYPE_SINT64:
+			fallthrough
+		case descriptor.FieldDescriptorProto_TYPE_INT64:
+			coltyp = coltype{
+				sqltype: "bigint",
+				gotype:  "int64",
+			}
+		case descriptor.FieldDescriptorProto_TYPE_BOOL:
+			coltyp = coltype{
+				sqltype: "boolean",
+				gotype:  "bool",
+			}
+		case descriptor.FieldDescriptorProto_TYPE_STRING:
+			coltyp = coltype{
+				sqltype: "bytea",
+				gotype:  "string",
+			}
+		case descriptor.FieldDescriptorProto_TYPE_MESSAGE:
+			coltyp = coltype{
+				sqltype: "bytea",
+				gotype:  typeNameToGoName(*f.TypeName, packmap),
+			}
+		case descriptor.FieldDescriptorProto_TYPE_BYTES:
+			coltyp = coltype{
+				sqltype: "bytea",
+				gotype:  "[]byte",
+			}
+		default:
+			return t, fmt.Errorf("No type for %s", f.Type)
+		}
+		col := &column{
+			name:   *f.Name,
+			coltyp: coltyp,
+			field:  f,
+		}
+		fieldNames[*f.Name] = col
+		t.columns = append(t.columns, col)
+		if *f.Name == "data" {
+			t.datagotype = typeNameToGoName(*f.TypeName, packmap)
+		}
+	}
+	if fieldNames["data"] == nil {
+		return t, fmt.Errorf("Missing data col on table %s", t.name)
+	}
+
+	for _, k := range opts.Key {
+		if len(k.Col) == 0 {
+			return t, fmt.Errorf("No cols in key on table %s", t.name)
+		}
+		var cols []*column
+		for _, c := range k.Col {
+			if fieldNames[c] == nil {
+				return t, fmt.Errorf("Unknown col on table %s", t.name)
+			}
+			cols = append(cols, fieldNames[c])
+		}
+		name := k.Name
+		var typ keyType
+		switch k.KeyType {
+		case model.KeyType_PRIMARY:
+			typ = primaryKey
+			if k.Name == "" {
+				name = "Primary"
+			}
+		case model.KeyType_UNIQUE:
+			typ = uniqueKey
+			if k.Name == "" {
+				return t, fmt.Errorf("Missing name for key on table %s", t.name)
+			}
+		case model.KeyType_INDEX:
+			typ = indexKey
+			if k.Name == "" {
+				return t, fmt.Errorf("Missing name for key on table %s", t.name)
+			}
+		default:
+			return t, fmt.Errorf("Unknown key type on table %s", t.name)
+		}
+		t.indexes = append(t.indexes, index{
+			name:    t.name + name,
+			keyType: typ,
+			columns: cols,
+		})
+	}
+
+	return t, nil
+}
+
+func typeNameToGoName(tn string, packmap map[string]string) string {
+	var best string
+	for k := range packmap {
+		if strings.HasPrefix(tn, "."+k+".") {
+			if len(k) > len(best) {
+				best = k
+			}
+		}
+	}
+	if best != "" {
+		msg := strings.TrimPrefix(tn, "."+best+".")
+		if gopackage := packmap[best]; gopackage != "" {
+			return gopackage + "." + strings.Join(strings.Split(msg, "."), "_")
+		}
+		return strings.Join(strings.Split(msg, "."), "_")
+	}
+	panic("Could not find type!" + tn)
 }
--- schema/tables/tables.tab.go
+++ schema/tables/tables.tab.go
@@ -5,19 +5,17 @@ import (
 
 	"github.com/golang/protobuf/proto"
 
-	"pixur.org/pixur/schema"
 	"pixur.org/pixur/schema/db"
-)
 
-type Job struct {
-	Tx *sql.Tx
-}
+	schema "pixur.org/pixur/schema"
+	_ "pixur.org/pixur/schema/db/model"
+)
 
 var SqlTables = []string{
 	"CREATE TABLE \"Pics\" (" +
 		"\"id\" bigint NOT NULL, " +
 		"\"created_time\" bigint NOT NULL, " +
-		"\"is_hidden\" smallint NOT NULL, " +
+		"\"is_hidden\" boolean NOT NULL, " +
 		"\"data\" bytea NOT NULL, " +
 		"PRIMARY KEY(\"id\")" +
 		");",
@@ -56,126 +54,316 @@ var SqlTables = []string{
 var _ db.Idx = PicsPrimary{}
 
 type PicsPrimary struct {
+	Id *int64
 }
 
 func (idx PicsPrimary) Cols() []string {
 	return []string{"id"}
 }
 
-func (idx PicsPrimary) Vals() []interface{} {
+func (idx PicsPrimary) Vals() (vals []interface{}) {
+	var done bool
+	if idx.Id != nil {
+		if done {
+			panic("Extra value Id")
+		}
+		vals = append(vals, *idx.Id)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = PicsBumpOrder{}
 
 type PicsBumpOrder struct {
+	CreatedTime *int64
 }
 
 func (idx PicsBumpOrder) Cols() []string {
 	return []string{"created_time"}
 }
 
-func (idx PicsBumpOrder) Vals() []interface{} {
+func (idx PicsBumpOrder) Vals() (vals []interface{}) {
+	var done bool
+	if idx.CreatedTime != nil {
+		if done {
+			panic("Extra value CreatedTime")
+		}
+		vals = append(vals, *idx.CreatedTime)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = PicsHidden{}
 
 type PicsHidden struct {
+	IsHidden *bool
 }
 
 func (idx PicsHidden) Cols() []string {
 	return []string{"is_hidden"}
 }
 
-func (idx PicsHidden) Vals() []interface{} {
+func (idx PicsHidden) Vals() (vals []interface{}) {
+	var done bool
+	if idx.IsHidden != nil {
+		if done {
+			panic("Extra value IsHidden")
+		}
+		vals = append(vals, *idx.IsHidden)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = TagsPrimary{}
 
 type TagsPrimary struct {
+	Id *int64
 }
 
 func (idx TagsPrimary) Cols() []string {
 	return []string{"id"}
 }
 
-func (idx TagsPrimary) Vals() []interface{} {
+func (idx TagsPrimary) Vals() (vals []interface{}) {
+	var done bool
+	if idx.Id != nil {
+		if done {
+			panic("Extra value Id")
+		}
+		vals = append(vals, *idx.Id)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = TagsName{}
 
 type TagsName struct {
+	Name *string
 }
 
 func (idx TagsName) Cols() []string {
 	return []string{"name"}
 }
 
-func (idx TagsName) Vals() []interface{} {
+func (idx TagsName) Vals() (vals []interface{}) {
+	var done bool
+	if idx.Name != nil {
+		if done {
+			panic("Extra value Name")
+		}
+		vals = append(vals, *idx.Name)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = PicTagsPrimary{}
 
 type PicTagsPrimary struct {
+	PicId *int64
+	TagId *int64
 }
 
 func (idx PicTagsPrimary) Cols() []string {
 	return []string{"pic_id", "tag_id"}
 }
 
-func (idx PicTagsPrimary) Vals() []interface{} {
+func (idx PicTagsPrimary) Vals() (vals []interface{}) {
+	var done bool
+	if idx.PicId != nil {
+		if done {
+			panic("Extra value PicId")
+		}
+		vals = append(vals, *idx.PicId)
+	} else {
+		done = true
+	}
+	if idx.TagId != nil {
+		if done {
+			panic("Extra value TagId")
+		}
+		vals = append(vals, *idx.TagId)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = PicIdentsPrimary{}
 
 type PicIdentsPrimary struct {
+	PicId *int64
+	Type  *schema.PicIdent_Type
+	Value *[]byte
 }
 
 func (idx PicIdentsPrimary) Cols() []string {
 	return []string{"pic_id", "type", "value"}
 }
 
-func (idx PicIdentsPrimary) Vals() []interface{} {
+func (idx PicIdentsPrimary) Vals() (vals []interface{}) {
+	var done bool
+	if idx.PicId != nil {
+		if done {
+			panic("Extra value PicId")
+		}
+		vals = append(vals, *idx.PicId)
+	} else {
+		done = true
+	}
+	if idx.Type != nil {
+		if done {
+			panic("Extra value Type")
+		}
+		vals = append(vals, *idx.Type)
+	} else {
+		done = true
+	}
+	if idx.Value != nil {
+		if done {
+			panic("Extra value Value")
+		}
+		vals = append(vals, *idx.Value)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = PicIdentsIdent{}
 
 type PicIdentsIdent struct {
+	Type  *schema.PicIdent_Type
+	Value *[]byte
 }
 
 func (idx PicIdentsIdent) Cols() []string {
 	return []string{"type", "value"}
 }
 
-func (idx PicIdentsIdent) Vals() []interface{} {
+func (idx PicIdentsIdent) Vals() (vals []interface{}) {
+	var done bool
+	if idx.Type != nil {
+		if done {
+			panic("Extra value Type")
+		}
+		vals = append(vals, *idx.Type)
+	} else {
+		done = true
+	}
+	if idx.Value != nil {
+		if done {
+			panic("Extra value Value")
+		}
+		vals = append(vals, *idx.Value)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = UsersPrimary{}
 
 type UsersPrimary struct {
+	Id *int64
 }
 
 func (idx UsersPrimary) Cols() []string {
 	return []string{"id"}
 }
 
-func (idx UsersPrimary) Vals() []interface{} {
+func (idx UsersPrimary) Vals() (vals []interface{}) {
+	var done bool
+	if idx.Id != nil {
+		if done {
+			panic("Extra value Id")
+		}
+		vals = append(vals, *idx.Id)
+	} else {
+		done = true
+	}
+	return
 }
 
 var _ db.Idx = UsersIdent{}
 
 type UsersIdent struct {
+	Ident *string
 }
 
 func (idx UsersIdent) Cols() []string {
 	return []string{"ident"}
 }
 
-func (idx UsersIdent) Vals() []interface{} {
+func (idx UsersIdent) Vals() (vals []interface{}) {
+	var done bool
+	if idx.Ident != nil {
+		if done {
+			panic("Extra value Ident")
+		}
+		vals = append(vals, *idx.Ident)
+	} else {
+		done = true
+	}
+	return
+}
+
+type Job struct {
+	Tx *sql.Tx
 }
 
 func (j Job) ScanPics(opts db.Opts, cb func(schema.Pic) error) error {
 	return db.Scan(j.Tx, "Pics", opts, func(data []byte) error {
 		var pb schema.Pic
+		if err := proto.Unmarshal(data, &pb); err != nil {
+			return err
+		}
+		return cb(pb)
+	})
+}
+
+func (j Job) ScanTags(opts db.Opts, cb func(schema.Tag) error) error {
+	return db.Scan(j.Tx, "Tags", opts, func(data []byte) error {
+		var pb schema.Tag
+		if err := proto.Unmarshal(data, &pb); err != nil {
+			return err
+		}
+		return cb(pb)
+	})
+}
+
+func (j Job) ScanPicTags(opts db.Opts, cb func(schema.PicTag) error) error {
+	return db.Scan(j.Tx, "PicTags", opts, func(data []byte) error {
+		var pb schema.PicTag
+		if err := proto.Unmarshal(data, &pb); err != nil {
+			return err
+		}
+		return cb(pb)
+	})
+}
+
+func (j Job) ScanPicIdents(opts db.Opts, cb func(schema.PicIdent) error) error {
+	return db.Scan(j.Tx, "PicIdents", opts, func(data []byte) error {
+		var pb schema.PicIdent
+		if err := proto.Unmarshal(data, &pb); err != nil {
+			return err
+		}
+		return cb(pb)
+	})
+}
+
+func (j Job) ScanUsers(opts db.Opts, cb func(schema.User) error) error {
+	return db.Scan(j.Tx, "Users", opts, func(data []byte) error {
+		var pb schema.User
 		if err := proto.Unmarshal(data, &pb); err != nil {
 			return err
 		}
@@ -191,32 +379,12 @@ func (j Job) FindPics(opts db.Opts) (rows []schema.Pic, err error) {
 	return
 }
 
-func (j Job) ScanTags(opts db.Opts, cb func(schema.Tag) error) error {
-	return db.Scan(j.Tx, "Tags", opts, func(data []byte) error {
-		var pb schema.Tag
-		if err := proto.Unmarshal(data, &pb); err != nil {
-			return err
-		}
-		return cb(pb)
-	})
-}
-
 func (j Job) FindTags(opts db.Opts) (rows []schema.Tag, err error) {
 	err = j.ScanTags(opts, func(data schema.Tag) error {
 		rows = append(rows, data)
 		return nil
 	})
 	return
-}
-
-func (j Job) ScanPicTags(opts db.Opts, cb func(schema.PicTag) error) error {
-	return db.Scan(j.Tx, "PicTags", opts, func(data []byte) error {
-		var pb schema.PicTag
-		if err := proto.Unmarshal(data, &pb); err != nil {
-			return err
-		}
-		return cb(pb)
-	})
 }
 
 func (j Job) FindPicTags(opts db.Opts) (rows []schema.PicTag, err error) {
@@ -227,32 +395,12 @@ func (j Job) FindPicTags(opts db.Opts) (rows []schema.PicTag, err error) {
 	return
 }
 
-func (j Job) ScanPicIdents(opts db.Opts, cb func(schema.PicIdent) error) error {
-	return db.Scan(j.Tx, "PicIdents", opts, func(data []byte) error {
-		var pb schema.PicIdent
-		if err := proto.Unmarshal(data, &pb); err != nil {
-			return err
-		}
-		return cb(pb)
-	})
-}
-
 func (j Job) FindPicIdents(opts db.Opts) (rows []schema.PicIdent, err error) {
 	err = j.ScanPicIdents(opts, func(data schema.PicIdent) error {
 		rows = append(rows, data)
 		return nil
 	})
 	return
-}
-
-func (j Job) ScanUsers(opts db.Opts, cb func(schema.User) error) error {
-	return db.Scan(j.Tx, "Users", opts, func(data []byte) error {
-		var pb schema.User
-		if err := proto.Unmarshal(data, &pb); err != nil {
-			return err
-		}
-		return cb(pb)
-	})
 }
 
 func (j Job) FindUsers(opts db.Opts) (rows []schema.User, err error) {
