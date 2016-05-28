a33f381fc8a791e2c07cb6889db7291b9b20c67f
Carl Mastrangelo <carl@carlmastrangelo.com>
2016-05-28 10:20:25 -0700

    handlers: implement get session handler

--- handlers/get_session_handler.go
+++ handlers/get_session_handler.go
@@ -4,7 +4,9 @@ import (
 	"crypto/rsa"
 	"database/sql"
 	"net/http"
+	"time"
 
+	"pixur.org/pixur/schema"
 	"pixur.org/pixur/tasks"
 )
 
@@ -20,7 +22,49 @@ type GetSessionHandler struct {
 }
 
 func (h *GetSessionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Unsupported Method", http.StatusMethodNotAllowed)
+		return
+	}
 
+	var task = &tasks.AuthUserTask{
+		DB:     h.DB,
+		Now:    time.Now,
+		Email:  r.FormValue("ident"),
+		Secret: r.FormValue("secret"),
+	}
+	runner := new(tasks.TaskRunner)
+	if err := runner.Run(task); err != nil {
+		returnTaskError(w, err)
+		return
+	}
+
+	enc := JwtEncoder{
+		PrivateKey: h.PrivateKey,
+		Now:        time.Now,
+		Expiration: time.Hour * 24 * 365 * 10,
+	}
+	jwt, err := enc.Encode(&JwtPayload{
+		Subject: schema.Varint(task.User.UserId).Encode(),
+	})
+	if err != nil {
+		returnTaskError(w, err)
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:     "jwt",
+		Value:    string(jwt),
+		Path:     "/api/",
+		Expires:  time.Now().Add(enc.Expiration),
+		Secure:   true,
+		HttpOnly: true,
+	})
+
+	resp := GetSessionResponse{
+		UserId: schema.Varint(task.User.UserId).Encode(),
+	}
+
+	returnProtoJSON(w, r, &resp)
 }
 
 func init() {
