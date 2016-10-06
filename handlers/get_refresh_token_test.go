fda127a3c7a511d6f9c5497b90fd9f41bfff3138
Carl Mastrangelo <carl@carlmastrangelo.com>
2016-10-05 19:55:07 -0700

    handlers: add refresh token tests

--- handlers/get_refresh_token_test.go
+++ handlers/get_refresh_token_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -9,7 +10,9 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
+	tspb "github.com/golang/protobuf/ptypes/timestamp"
 
 	"pixur.org/pixur/schema"
 	"pixur.org/pixur/status"
@@ -248,7 +251,220 @@ func TestGetRefreshTokenSucceedsOnRefreshToken(t *testing.T) {
 	}
 }
 
+func TestGetRefreshTokenFailsOnInvalidToken(t *testing.T) {
+	h := &GetRefreshTokenHandler{}
+	_, sts := h.GetRefreshToken(context.Background(), &GetRefreshTokenRequest{
+		RefreshToken: "invalid",
+	})
+
+	if have, want := sts.Code(), status.Code_UNAUTHENTICATED; have != want {
+		t.Error("have", have, "want", want)
+	}
+
+	if have, want := sts.Message(), "can't decode token"; !strings.Contains(have, want) {
+		t.Error("have", have, "want", want)
+	}
+}
+
+func TestGetRefreshTokenFailsOnNonRefreshToken(t *testing.T) {
+	h := &GetRefreshTokenHandler{}
+	notafter, _ := ptypes.TimestampProto(time.Now().Add(refreshPwtDuration))
+	notbefore, _ := ptypes.TimestampProto(time.Now().Add(-1 * time.Minute))
+	payload := &PwtPayload{
+		Subject:   "9",
+		NotAfter:  notafter,
+		NotBefore: notbefore,
+		Type:      PwtPayload_AUTH,
+	}
+	refreshToken, err := defaultPwtCoder.encode(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	_, sts := h.GetRefreshToken(context.Background(), &GetRefreshTokenRequest{
+		RefreshToken: string(refreshToken),
+	})
+
+	if have, want := sts.Code(), status.Code_UNAUTHENTICATED; have != want {
+		t.Error("have", have, "want", want)
+	}
+
+	if have, want := sts.Message(), "can't decode non refresh token"; !strings.Contains(have, want) {
+		t.Error("have", have, "want", want)
+	}
+}
+
+func TestGetRefreshTokenFailsOnBadSubject(t *testing.T) {
+	h := &GetRefreshTokenHandler{}
+	notafter, _ := ptypes.TimestampProto(time.Now().Add(refreshPwtDuration))
+	notbefore, _ := ptypes.TimestampProto(time.Now().Add(-1 * time.Minute))
+	payload := &PwtPayload{
+		Subject:   "invalid",
+		NotAfter:  notafter,
+		NotBefore: notbefore,
+		Type:      PwtPayload_REFRESH,
+	}
+	refreshToken, err := defaultPwtCoder.encode(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	_, sts := h.GetRefreshToken(context.Background(), &GetRefreshTokenRequest{
+		RefreshToken: string(refreshToken),
+	})
+
+	if have, want := sts.Code(), status.Code_UNAUTHENTICATED; have != want {
+		t.Error("have", have, "want", want)
+	}
+
+	if have, want := sts.Message(), "can't decode subject"; !strings.Contains(have, want) {
+		t.Error("have", have, "want", want)
+	}
+}
+
+func TestGetRefreshTokenFailsOnTaskError(t *testing.T) {
+	failureRunner := func(task tasks.Task) status.S {
+		return status.InternalError(nil, "bad")
+	}
+	h := &GetRefreshTokenHandler{
+		Runner: tasks.TestTaskRunner(failureRunner),
+	}
+
+	_, sts := h.GetRefreshToken(context.Background(), &GetRefreshTokenRequest{})
+
+	if have, want := sts.Code(), status.Code_INTERNAL_ERROR; have != want {
+		t.Error("have", have, "want", want)
+	}
+
+	if have, want := sts.Message(), "bad"; !strings.Contains(have, want) {
+		t.Error("have", have, "want", want)
+	}
+}
+
+func TestGetRefreshToken(t *testing.T) {
+	var taskCap *tasks.AuthUserTask
+	successRunner := func(task tasks.Task) status.S {
+		taskCap = task.(*tasks.AuthUserTask)
+		taskCap.User = &schema.User{
+			UserId: 2,
+		}
+		taskCap.NewTokenID = 4
+		return nil
+	}
+	notafter, _ := ptypes.TimestampProto(time.Now().Add(refreshPwtDuration))
+	notbefore, _ := ptypes.TimestampProto(time.Now().Add(-1 * time.Minute))
+	payload := &PwtPayload{
+		Subject:   "2",
+		NotAfter:  notafter,
+		NotBefore: notbefore,
+		Type:      PwtPayload_REFRESH,
+		TokenId:   3,
+	}
+	refreshToken, err := defaultPwtCoder.encode(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	h := &GetRefreshTokenHandler{
+		Runner: tasks.TestTaskRunner(successRunner),
+		Now:    time.Now,
+	}
+
+	resp, sts := h.GetRefreshToken(context.Background(), &GetRefreshTokenRequest{
+		Ident:        "ident",
+		Secret:       "secret",
+		RefreshToken: string(refreshToken),
+	})
+	if sts != nil {
+		t.Fatal(err)
+	}
+
+	if have, want := taskCap.Email, "ident"; have != want {
+		t.Error("have", have, "want", want)
+	}
+	if have, want := taskCap.Secret, "secret"; have != want {
+		t.Error("have", have, "want", want)
+	}
+	if have, want := taskCap.UserID, int64(2); have != want {
+		t.Error("have", have, "want", want)
+	}
+	if have, want := taskCap.TokenID, int64(3); have != want {
+		t.Error("have", have, "want", want)
+	}
+
+	if len(resp.RefreshToken) == 0 || len(resp.AuthToken) == 0 || len(resp.PixToken) == 0 {
+		t.Error("expected non-empty token", resp.RefreshToken, resp.AuthToken, resp.PixToken)
+	}
+
+	if !withinProto(resp.RefreshPayload.NotBefore, time.Now(), time.Minute*2) {
+		t.Error("wrong before", resp.RefreshPayload.NotBefore)
+	}
+	if !withinProto(resp.RefreshPayload.NotAfter, time.Now().Add(refreshPwtDuration), time.Minute) {
+		t.Error("wrong after", resp.RefreshPayload.NotAfter)
+	}
+	resp.RefreshPayload.NotBefore = nil
+	resp.RefreshPayload.NotAfter = nil
+	expectedRefresh := &PwtPayload{
+		Subject: "2",
+		TokenId: 4,
+		Type:    PwtPayload_REFRESH,
+	}
+	if !proto.Equal(resp.RefreshPayload, expectedRefresh) {
+		t.Error("have", resp.RefreshPayload, "want", expectedRefresh)
+	}
+
+	if !withinProto(resp.AuthPayload.NotBefore, time.Now(), time.Minute*2) {
+		t.Error("wrong before", resp.AuthPayload.NotBefore)
+	}
+	if !withinProto(resp.AuthPayload.NotAfter, time.Now().Add(authPwtDuration), time.Minute) {
+		t.Error("wrong after", resp.AuthPayload.NotAfter)
+	}
+	resp.AuthPayload.NotBefore = nil
+	resp.AuthPayload.NotAfter = nil
+	expectedAuth := &PwtPayload{
+		Subject:       "2",
+		TokenParentId: 4,
+		Type:          PwtPayload_AUTH,
+	}
+	if !proto.Equal(resp.AuthPayload, expectedAuth) {
+		t.Error("have", resp.AuthPayload, "want", expectedAuth)
+	}
+
+	if !withinProto(resp.PixPayload.NotBefore, time.Now(), time.Minute*2) {
+		t.Error("wrong before", resp.PixPayload.NotBefore)
+	}
+	if !withinProto(resp.PixPayload.NotAfter, time.Now().Add(refreshPwtDuration), time.Minute) {
+		t.Error("wrong after", resp.PixPayload.NotAfter)
+	}
+	if !withinProto(resp.PixPayload.SoftNotAfter, time.Now().Add(authPwtDuration), time.Minute) {
+		t.Error("wrong soft after", resp.PixPayload.SoftNotAfter)
+	}
+	resp.PixPayload.NotBefore = nil
+	resp.PixPayload.NotAfter = nil
+	resp.PixPayload.SoftNotAfter = nil
+	expectedPix := &PwtPayload{
+		Subject:       "2",
+		TokenParentId: 4,
+		Type:          PwtPayload_PIX,
+	}
+	if !proto.Equal(resp.PixPayload, expectedPix) {
+		t.Error("have", resp.PixPayload, "want", expectedPix)
+	}
+}
+
 func within(t1, t2 time.Time, diff time.Duration) bool {
+	d := t1.Sub(t2)
+	if d < 0 {
+		d = -d
+	}
+	return d <= diff
+}
+
+func withinProto(t1pb *tspb.Timestamp, t2 time.Time, diff time.Duration) bool {
+	t1, err := ptypes.Timestamp(t1pb)
+	if err != nil {
+		panic(err)
+	}
 	d := t1.Sub(t2)
 	if d < 0 {
 		d = -d
