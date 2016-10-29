cba9bea8f93a9007bfe5071a358b7ce6ce973148
Carl Mastrangelo <carl@carlmastrangelo.com>
2016-10-28 22:12:04 -0700

    handlers: add handler to create comments

--- handlers/add_pic_comment_handler.go
+++ handlers/add_pic_comment_handler.go
@@ -11,6 +11,7 @@ import (
 	"pixur.org/pixur/tasks"
 )
 
+// TODO: add tests
 type AddPicCommentHandler struct {
 	// embeds
 	http.Handler
@@ -59,5 +60,41 @@ func (h *AddPicCommentHandler) AddPicComment(ctx context.Context, req *AddPicCom
 	return &AddPicCommentResponse{
 		Comment: apiPicComment(task.PicComment),
 	}, nil
+}
 
+func (h *AddPicCommentHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	rc := &requestChecker{r: r, now: h.Now}
+	rc.checkPost()
+	rc.checkXsrf()
+	if rc.sts != nil {
+		httpError(w, rc.sts)
+		return
+	}
+
+	ctx := r.Context()
+	if token, present := authTokenFromReq(r); present {
+		ctx = tasks.CtxFromAuthToken(ctx, token)
+	}
+
+	resp, sts := h.AddPicComment(ctx, &AddPicCommentRequest{
+		PicId:           r.FormValue("pic_id"),
+		CommentParentId: r.FormValue("comment_parent_id"),
+		Text:            r.FormValue("text"),
+	})
+
+	if sts != nil {
+		httpError(w, sts)
+		return
+	}
+
+	returnProtoJSON(w, r, resp)
+}
+
+func init() {
+	register(func(mux *http.ServeMux, c *ServerConfig) {
+		mux.Handle("/api/addPicComment", &AddPicCommentHandler{
+			DB:  c.DB,
+			Now: time.Now,
+		})
+	})
 }
