58ebfda8b0f004a70afc0781f0b3e62f512c934a
Carl Mastrangelo <carl@carlmastrangelo.com>
2016-12-01 21:46:27 -0800

    tasks: add a task to update users

---
+++ tasks/update_user_task.go
@@ -0,0 +1,105 @@
+package tasks
+
+import (
+	"context"
+	"time"
+
+	"pixur.org/pixur/schema"
+	"pixur.org/pixur/schema/db"
+	tab "pixur.org/pixur/schema/tables"
+	"pixur.org/pixur/status"
+)
+
+type UpdateUserTask struct {
+	// Deps
+	DB  db.DB
+	Now func() time.Time
+
+	// Inputs
+	ObjectUserID int64
+	Version      int64
+
+	// nil for no update, empty for clearing.
+	NewCapability []schema.User_Capability
+
+	Ctx context.Context
+}
+
+// TODO: Add tests
+func (t *UpdateUserTask) Run() (errCap status.S) {
+	j, err := tab.NewJob(t.DB)
+	if err != nil {
+		return status.InternalError(err, "Unable to Begin TX")
+	}
+	defer cleanUp(j, &errCap)
+
+	subjectUserID, ok := UserIDFromCtx(t.Ctx)
+	if !ok {
+		return status.Unauthenticated(nil, "missing user")
+	}
+
+	var subjectUser, objectUser *schema.User
+	if subjectUserID == t.ObjectUserID || t.ObjectUserID == 0 {
+		// modifying self
+		users, err := j.FindUsers(db.Opts{
+			Prefix: tab.UsersPrimary{&subjectUserID},
+			Lock:   db.LockWrite,
+		})
+		if err != nil {
+			return status.InternalError(err, "can't lookup user")
+		}
+		if len(users) != 1 {
+			return status.Unauthenticated(nil, "can't lookup user")
+		}
+		subjectUser = users[0]
+		objectUser = subjectUser
+	} else {
+		subjectUsers, err := j.FindUsers(db.Opts{
+			Prefix: tab.UsersPrimary{&subjectUserID},
+		})
+		if err != nil {
+			return status.InternalError(err, "can't lookup user")
+		}
+		if len(subjectUsers) != 1 {
+			return status.Unauthenticated(nil, "can't lookup user")
+		}
+		subjectUser = subjectUsers[0]
+
+		objectUsers, err := j.FindUsers(db.Opts{
+			Prefix: tab.UsersPrimary{&t.ObjectUserID},
+			Lock:   db.LockWrite,
+		})
+		if err != nil {
+			return status.InternalError(err, "can't lookup user")
+		}
+		if len(objectUsers) != 1 {
+			return status.Unauthenticated(nil, "can't lookup user")
+		}
+		objectUser = objectUsers[0]
+	}
+
+	if !schema.UserHasPerm(subjectUser, schema.User_USER_UPDATE_CAPABILITY) {
+		return status.PermissionDeniedf(nil, "missing capability")
+	}
+
+	if objectUser.Version() != t.Version {
+		return status.Aborted(nil, "version mismatch")
+	}
+
+	now := t.Now()
+	objectUser.ModifiedTs = schema.ToTs(now)
+
+	if t.NewCapability != nil {
+		objectUser.Capability = t.NewCapability
+	}
+
+	if err := j.UpdateUser(objectUser); err != nil {
+		return status.InternalError(err, "can't update user")
+	}
+
+	if err := j.Commit(); err != nil {
+		return status.InternalError(err, "can't commit")
+	}
+
+	return nil
+}
