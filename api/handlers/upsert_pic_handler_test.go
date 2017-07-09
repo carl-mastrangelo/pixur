83c5af65e250eae742692a353aac053f2d08d9eb
Carl Mastrangelo <carl@carlmastrangelo.com>
2017-07-09 00:48:28 -0700

    api/handlers: add upsert pic tests

---
+++ api/handlers/upsert_pic_handler_test.go
@@ -0,0 +1,77 @@
+package handlers
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"pixur.org/pixur/api"
+	"pixur.org/pixur/schema"
+	"pixur.org/pixur/status"
+	"pixur.org/pixur/tasks"
+)
+
+func TestUpsertPicFailsOnBadMd5(t *testing.T) {
+	s := &serv{}
+	_, sts := s.handleUpsertPic(context.Background(), &api.UpsertPicRequest{
+		Md5Hash: []byte("x"),
+	})
+	if sts == nil {
+		t.Fatal("nil status")
+	}
+	if have, want := sts.Code(), status.Code_INVALID_ARGUMENT; have != want {
+		t.Error("have", have, "want", want)
+	}
+	if have, want := sts.Message(), "bad md5 hash"; !strings.Contains(have, want) {
+		t.Error("have", have, "want", want)
+	}
+}
+
+func TestUpsertPic(t *testing.T) {
+	var taskCap *tasks.UpsertPicTask
+	successRunner := func(task tasks.Task) status.S {
+		taskCap = task.(*tasks.UpsertPicTask)
+		taskCap.CreatedPic = new(schema.Pic)
+		return nil
+	}
+	s := &serv{
+		runner: tasks.TestTaskRunner(successRunner),
+		now:    time.Now,
+	}
+	res, sts := s.handleUpsertPic(context.Background(), &api.UpsertPicRequest{
+		Url:     "http://foo/",
+		Data:    []byte("a"),
+		Name:    "bar",
+		Md5Hash: []byte("0123456789abcdef"),
+		Tag:     []string{"blah"},
+	})
+	if sts != nil {
+		t.Fatal(sts)
+	}
+	if taskCap == nil {
+		t.Fatal("task didn't run")
+	}
+	if have, want := taskCap.FileURL, "http://foo/"; have != want {
+		t.Error("have", have, "want", want)
+	}
+	if taskCap.File == nil {
+		t.Error("file is nil")
+	}
+	if taskCap.HTTPClient == nil || taskCap.TempFile == nil || taskCap.Rename == nil ||
+		taskCap.MkdirAll == nil || taskCap.Now == nil {
+		t.Error("deps are nil", taskCap)
+	}
+	if len(taskCap.TagNames) != 1 || taskCap.TagNames[0] != "blah" {
+		t.Error("bad tag names", taskCap.TagNames)
+	}
+	if have, want := taskCap.Ctx, context.Background(); have != want {
+		t.Error("have", have, "want", want)
+	}
+	if len(taskCap.Md5Hash) != 16 {
+		t.Error("bad md5 hash", taskCap.Md5Hash)
+	}
+	if res == nil {
+		t.Error("bad response")
+	}
+}
