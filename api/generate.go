b25d76a886d7de7f2c84de6d2bda35b0f4a13654
Carl Mastrangelo <carl@carlmastrangelo.com>
2018-03-17 21:22:17 -0700

    api: add api version to service options

--- api/generate.go
+++ api/generate.go
@@ -1,2 +1,9 @@
-//go:generate protoc api.proto --go_out=plugins=grpc:.
+//go:generate /bin/sh -e -c "tail -n +5 ./generate.go | /bin/sh -e"
 package api
+
+/*
+sed -i "s/api_version: [0-9]\\+/api_version: `date +%Y%m%d`/" api.proto \
+&& protoc api.proto --go_out=plugins=grpc:.
+
+exit 0
+*/
