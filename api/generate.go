526b69759b100a07af114b474623355d33c023fd
Carl Mastrangelo <carl@carlmastrangelo.com>
2018-07-05 11:58:05 -0700

    all: make command package pixur.org

--- api/generate.go
+++ api/generate.go
@@ -1,5 +1,7 @@
 //go:generate /bin/sh -e -c "tail -n +5 ./generate.go | /bin/sh -e"
-package api
+
+// Package api contains the Protobuf message used to communicate with the backend server.
+package api // import "pixur.org/pixur/api"
 
 /*
 sed -i "s/api_version: [0-9]\\+/api_version: `date +%Y%m%d`/" api.proto \
