6f06e718aab97ea97f2ff49ca05a9f526715b437
Carl Mastrangelo <carl@carlmastrangelo.com>
2018-10-15 22:16:50 -0700

    be/schema: add thumbnail helper function

--- be/schema/picfile.go
+++ be/schema/picfile.go
@@ -7,6 +7,37 @@ import (
 	"pixur.org/pixur/be/status"
 )
 
+var picFileMimeExt = map[Pic_File_Mime]string{
+	Pic_File_JPEG: ".jpg",
+	Pic_File_GIF:  ".gif",
+	Pic_File_PNG:  ".png",
+	Pic_File_WEBM: ".webm",
+}
+
+var picFileMimeTypes = map[string]Pic_File_Mime{
+	".jpg":  Pic_File_JPEG,
+	".gif":  Pic_File_GIF,
+	".png":  Pic_File_PNG,
+	".webm": Pic_File_WEBM,
+}
+
+func init() {
+	if len(picFileMimeExt) != len(Pic_File_Mime_name)-1 {
+		panic("mime map wrong")
+	}
+	if len(picFileMimeTypes) != len(Pic_File_Mime_name)-1 {
+		panic("mime map wrong")
+	}
+	for k, _ := range Pic_File_Mime_name {
+		if Pic_File_Mime(k) == Pic_File_UNKNOWN {
+			continue
+		}
+		if _, present := picFileMimeExt[Pic_File_Mime(k)]; !present {
+			panic("missing value in mime map")
+		}
+	}
+}
+
 func PicFilePath(pixPath, picFileID string, format Pic_Mime) (string, status.S) {
 	if filepath.Base(picFileID) != picFileID {
 		return "", status.InvalidArgument(nil, "not a varint")
@@ -24,4 +55,16 @@ func PicFilePath(pixPath, picFileID string, format Pic_Mime) (string, status.S) 
 	path = append(path, id+"."+format.Ext())
 
 	return filepath.Join(path...), nil
+}
+
+func PicFileThumbnailPath(
+	pixPath string, picFileID, index int64, mime Pic_File_Mime) (string, status.S) {
+	ext, present := picFileMimeExt[mime]
+	if !present {
+		return "", status.InvalidArgument(nil, "unknown mime", mime)
+	}
+
+	return filepath.Join(
+		PicBaseDir(pixPath, picFileID),
+		Varint(picFileID).Encode()+Varint(index).Encode()+ext), nil
 }
