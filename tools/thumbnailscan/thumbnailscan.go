adf9b5793f72d075102f517d5efdce8158bf0e04
Carl Mastrangelo <carl@carlmastrangelo.com>
2018-10-16 21:49:52 -0700

    tools: update thumbnail scan to delete old fields

--- tools/thumbnailscan/thumbnailscan.go
+++ tools/thumbnailscan/thumbnailscan.go
@@ -3,10 +3,7 @@ package main
 import (
 	"context"
 	"flag"
-	"io/ioutil"
 	"log"
-	"os"
-	"time"
 
 	"pixur.org/pixur/be/imaging"
 	"pixur.org/pixur/be/schema"
@@ -32,103 +29,15 @@ func handlePic(db sdb.DB, picId int64) status.S {
 	}
 	p := pics[0]
 
-	if !p.HardDeleted() && len(p.Thumbnail) != 1 {
-		panic("bad pic")
-	}
-
-	if p.HardDeleted() || p.Thumbnail[0].Width == 192 {
+	if p.FileSize == 0 {
 		return nil
 	}
-	log.Println(p)
+	p.FileSize = 0
+	p.Mime = schema.Pic_UNKNOWN
+	p.Width = 0
+	p.Height = 0
+	p.AnimationInfo = nil
 
-	if !p.HardDeleted() {
-		path, sts := schema.PicFilePath(beconfig.Conf.PixPath, p.PicId, p.File.Mime)
-		if sts != nil {
-			return sts
-		}
-
-		f, err := os.Open(path)
-		if err != nil {
-			return status.InternalError(err, "can't open", p)
-		}
-		defer f.Close()
-		im, sts := imaging.ReadImage(f)
-		if sts != nil {
-			return sts
-		}
-		defer im.Close()
-
-		imt, sts := im.Thumbnail()
-		if sts != nil {
-			return sts
-		}
-		defer imt.Close()
-
-		ft, err := ioutil.TempFile(beconfig.Conf.PixPath, "__oo")
-		if err != nil {
-			return status.InternalError(err, "can't make tempfile")
-		}
-		defer os.Remove(ft.Name())
-		defer func() {
-			if err := ft.Close(); err != nil {
-				log.Println(err, "failed to close", p)
-			}
-		}()
-
-		if sts := imt.Write(ft); sts != nil {
-			return sts
-		}
-		fti, err := ft.Stat()
-		if err != nil {
-			return status.InternalError(err, "can't stat tempfile")
-		}
-		mimet, sts := imageFormatToMime(imt.Format())
-		if sts != nil {
-			return sts
-		}
-		wt, ht := imt.Dimensions()
-		now := time.Now()
-		nowts := schema.ToTspb(now)
-
-		oldthumbnailpath, sts := schema.PicFileThumbnailPath(
-			beconfig.Conf.PixPath, p.PicId, p.Thumbnail[0].Index, p.Thumbnail[0].Mime)
-		if sts != nil {
-			return sts
-		}
-		p.Thumbnail = nil
-
-		p.Thumbnail = append(p.Thumbnail, &schema.Pic_File{
-			Index:         0,
-			Size:          fti.Size(),
-			Mime:          mimet,
-			Width:         int64(wt),
-			Height:        int64(ht),
-			CreatedTs:     nowts,
-			ModifiedTs:    nowts,
-			AnimationInfo: nil,
-		})
-
-		if err := ft.Close(); err != nil {
-			return status.InternalError(err, "can't close old thumbnail")
-		}
-
-		newthumbnailpath, sts := schema.PicFileThumbnailPath(
-			beconfig.Conf.PixPath, p.PicId, p.Thumbnail[0].Index, p.Thumbnail[0].Mime)
-		if sts != nil {
-			return sts
-		}
-		if err := os.Remove(oldthumbnailpath); err != nil {
-			if os.IsNotExist(err) {
-				log.Println(err, "No thumbnail found for ", p.PicId)
-			} else {
-				return status.InternalError(err, "can't remove old path")
-			}
-		}
-		if err := os.Rename(ft.Name(), newthumbnailpath); err != nil {
-			return status.InternalError(err, "can't rename thumbnail")
-		}
-	}
-	log.Println(p)
 	if err := j.UpdatePic(p); err != nil {
 		return status.InternalError(err, "can't update pic")
 	}
@@ -136,6 +45,7 @@ func handlePic(db sdb.DB, picId int64) status.S {
 	if err := j.Commit(); err != nil {
 		return status.InternalError(err, "can't commit")
 	}
+	log.Println(p.GetVarPicID())
 
 	return nil
 }
