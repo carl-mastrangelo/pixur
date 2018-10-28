b011b54bbf776b507237c93eb86203b176897c92
Carl Mastrangelo <carl@carlmastrangelo.com>
2018-10-28 13:06:09 -0700

    be/tasks: move tag creation logic to addpictags task

--- be/tasks/add_pic_tags_task.go
+++ be/tasks/add_pic_tags_task.go
@@ -2,6 +2,7 @@ package tasks
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"pixur.org/pixur/be/schema"
@@ -55,6 +56,205 @@ func (t *AddPicTagsTask) Run(ctx context.Context) (stscap status.S) {
 
 	if err := j.Commit(); err != nil {
 		return status.Internal(err, "can't commit job")
+	}
+	return nil
+}
+
+type tagNameAndUniq struct {
+	name, uniq string
+}
+
+func upsertTags(j *tab.Job, rawTags []string, picID int64, now time.Time, userID int64) status.S {
+	newTagNames, sts := cleanTagNames(rawTags)
+	if sts != nil {
+		return sts
+	}
+
+	attachedTags, _, sts := findAttachedPicTags(j, picID)
+	if sts != nil {
+		return sts
+	}
+
+	unattachedTagNames := findUnattachedTagNames(attachedTags, newTagNames)
+	existingTags, unknownNames, sts := findExistingTagsByName(j, unattachedTagNames)
+	if sts != nil {
+		return sts
+	}
+
+	if sts := updateExistingTags(j, existingTags, now); sts != nil {
+		return sts
+	}
+	newTags, sts := createNewTags(j, unknownNames, now)
+	if sts != nil {
+		return sts
+	}
+
+	existingTags = append(existingTags, newTags...)
+	if _, sts := createPicTags(j, existingTags, picID, now, userID); sts != nil {
+		return sts
+	}
+
+	return nil
+}
+
+func findAttachedPicTags(j *tab.Job, picID int64) ([]*schema.Tag, []*schema.PicTag, status.S) {
+	pts, err := j.FindPicTags(db.Opts{
+		Prefix: tab.PicTagsPrimary{PicId: &picID},
+		Lock:   db.LockWrite,
+	})
+	if err != nil {
+		return nil, nil, status.Internal(err, "cant't find pic tags")
+	}
+
+	var tags []*schema.Tag
+	// TODO: maybe do something with lock ordering?
+	for _, pt := range pts {
+		ts, err := j.FindTags(db.Opts{
+			Prefix: tab.TagsPrimary{&pt.TagId},
+			Limit:  1,
+			Lock:   db.LockWrite,
+		})
+		if err != nil {
+			return nil, nil, status.Internal(err, "can't find tags")
+		}
+		if len(ts) != 1 {
+			return nil, nil, status.Internal(nil, "can't lookup tag", len(ts))
+		}
+		tags = append(tags, ts[0])
+	}
+	return tags, pts, nil
+}
+
+// findUnattachedTagNames finds tag names that are not part of a pic's tags.
+// While pic tags are the SoT for attachment, only the Tag is the SoT for the name.
+func findUnattachedTagNames(attachedTags []*schema.Tag, newTagNames []tagNameAndUniq) []tagNameAndUniq {
+	attachedTagNames := make(map[string]struct{}, len(attachedTags))
+
+	for _, tag := range attachedTags {
+		attachedTagNames[schema.TagUniqueName(tag.Name)] = struct{}{}
+	}
+	var unattachedTagNames []tagNameAndUniq
+	for _, newTagName := range newTagNames {
+		if _, attached := attachedTagNames[newTagName.uniq]; !attached {
+			unattachedTagNames = append(unattachedTagNames, newTagName)
+		}
+	}
+
+	return unattachedTagNames
+}
+
+func findExistingTagsByName(j *tab.Job, names []tagNameAndUniq) (
+	tags []*schema.Tag, unknownNames []tagNameAndUniq, _ status.S) {
+	for _, name := range names {
+		ts, err := j.FindTags(db.Opts{
+			Prefix: tab.TagsName{&name.uniq},
+			Limit:  1,
+			Lock:   db.LockWrite,
+		})
+		if err != nil {
+			return nil, nil, status.Internal(err, "can't find tags")
+		}
+		if len(ts) == 1 {
+			tags = append(tags, ts[0])
+		} else {
+			unknownNames = append(unknownNames, name)
+		}
+	}
+
+	return
+}
+
+func updateExistingTags(j *tab.Job, tags []*schema.Tag, now time.Time) status.S {
+	for _, tag := range tags {
+		tag.SetModifiedTime(now)
+		tag.UsageCount++
+		if err := j.UpdateTag(tag); err != nil {
+			return status.Internal(err, "can't update tag")
+		}
+	}
+	return nil
+}
+
+func createNewTags(j *tab.Job, names []tagNameAndUniq, now time.Time) ([]*schema.Tag, status.S) {
+	var tags []*schema.Tag
+	for _, name := range names {
+		tagID, err := j.AllocID()
+		if err != nil {
+			return nil, status.Internal(err, "can't allocate id")
+		}
+		tag := &schema.Tag{
+			TagId:      tagID,
+			Name:       name.name,
+			UsageCount: 1,
+		}
+		tag.SetCreatedTime(now)
+		tag.SetModifiedTime(now)
+		if err := j.InsertTag(tag); err != nil {
+			return nil, status.Internal(err, "can't create tag")
+		}
+		tags = append(tags, tag)
+	}
+	return tags, nil
+}
+
+func createPicTags(j *tab.Job, tags []*schema.Tag, picID int64, now time.Time, userID int64) (
+	[]*schema.PicTag, status.S) {
+	var picTags []*schema.PicTag
+	for _, tag := range tags {
+		pt := &schema.PicTag{
+			PicId:  picID,
+			TagId:  tag.TagId,
+			Name:   tag.Name,
+			UserId: userID,
+		}
+		pt.SetCreatedTime(now)
+		pt.SetModifiedTime(now)
+		if err := j.InsertPicTag(pt); err != nil {
+			return nil, status.Internal(err, "can't create pic tag")
+		}
+		picTags = append(picTags, pt)
+	}
+	return picTags, nil
+}
+
+func cleanTagNames(rawTagNames []string) ([]tagNameAndUniq, status.S) {
+	validtagnames := make([]tagNameAndUniq, 0, len(rawTagNames))
+	for _, rawtagname := range rawTagNames {
+		if sts := validateMaxLength(rawtagname, "tag", minTagLength, maxTagLength); sts != nil {
+			return nil, sts
+		}
+		if sts := validateUtf8(rawtagname, "tag"); sts != nil {
+			return nil, sts
+		}
+		normalrawtagname := normalizeUnicodeTextUnsafe(rawtagname)
+		trimmednormaltagname := strings.TrimSpace(normalrawtagname)
+
+		if sts := validateMaxLength(trimmednormaltagname, "tag", minTagLength, maxTagLength); sts != nil {
+			return nil, sts
+		}
+		if sts := validateGraphicText(trimmednormaltagname, "tag"); sts != nil {
+			return nil, sts
+		}
+		validtagnames = append(validtagnames, tagNameAndUniq{
+			name: trimmednormaltagname,
+			uniq: schema.TagUniqueName(trimmednormaltagname),
+		})
+	}
+
+	if sts := validateNoDuplicateTags(validtagnames); sts != nil {
+		return nil, sts
+	}
+
+	return validtagnames, nil
+}
+
+func validateNoDuplicateTags(tagNames []tagNameAndUniq) status.S {
+	var seen = make(map[string]int, len(tagNames))
+	for i, tn := range tagNames {
+		if pos, present := seen[tn.uniq]; present {
+			return status.InvalidArgumentf(nil, "duplicate tag '%s' at position %d and %d", tn.name, pos, i)
+		}
+		seen[tn.uniq] = i
 	}
 	return nil
 }
---
+++ be/tasks/add_pic_tags_task_test.go
@@ -0,0 +1,359 @@
+package tasks
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+
+	"pixur.org/pixur/be/schema"
+	"pixur.org/pixur/be/status"
+)
+
+func TestUpsertTags(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	attachedTag := c.CreateTag()
+	unattachedTag := c.CreateTag()
+	pic := c.CreatePic()
+	c.CreatePicTag(pic, attachedTag)
+
+	j := c.Job()
+	defer j.Rollback()
+
+	now := time.Now()
+	tagNames := []string{attachedTag.Tag.Name, unattachedTag.Tag.Name, "missing"}
+	err := upsertTags(j, tagNames, pic.Pic.PicId, now, -1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	allTags, allPicTags, err := findAttachedPicTags(j, pic.Pic.PicId)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(allTags) != 3 || len(allPicTags) != 3 {
+		t.Fatal("not all tags created", allTags, allPicTags)
+	}
+}
+
+func TestCreatePicTags(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	tag := c.CreateTag()
+	pic := c.CreatePic()
+	now := time.Now()
+
+	j := c.Job()
+	defer j.Rollback()
+
+	picTags, err := createPicTags(j, []*schema.Tag{tag.Tag}, pic.Pic.PicId, now, -1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedPicTag := &schema.PicTag{
+		PicId:  pic.Pic.PicId,
+		TagId:  tag.Tag.TagId,
+		Name:   tag.Tag.Name,
+		UserId: -1,
+	}
+	expectedPicTag.SetCreatedTime(now)
+	expectedPicTag.SetModifiedTime(now)
+
+	if len(picTags) != 1 || !proto.Equal(picTags[0], expectedPicTag) {
+		t.Fatal("Pic tags mismatch", picTags, expectedPicTag)
+	}
+}
+
+func TestCreatePicTags_CantPrepare(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	tag := c.CreateTag()
+	pic := c.CreatePic()
+	now := time.Now()
+
+	j := c.Job()
+	j.Rollback()
+
+	_, sts := createPicTags(j, []*schema.Tag{tag.Tag}, pic.Pic.PicId, now, schema.AnonymousUserID)
+	expected := status.Internal(nil, "can't create pic tag")
+	compareStatus(t, sts, expected)
+}
+
+func TestCreateNewTags(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	j := c.Job()
+	defer j.Rollback()
+
+	now := time.Now()
+
+	newTags, err := createNewTags(j, []tagNameAndUniq{{name: "A", uniq: "a"}}, now)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(newTags) != 1 {
+		t.Fatal("Didn't create tag", newTags)
+	}
+
+	expectedTag := &schema.Tag{
+		TagId:      newTags[0].TagId,
+		Name:       "A",
+		UsageCount: 1,
+	}
+	expectedTag.SetCreatedTime(now)
+	expectedTag.SetModifiedTime(now)
+	if !proto.Equal(newTags[0], expectedTag) {
+		t.Fatal("tag not expected", newTags[0], expectedTag)
+	}
+}
+
+func TestCreateNewTags_CantCreate(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	j := c.Job()
+	j.Rollback()
+
+	now := time.Now()
+
+	_, sts := createNewTags(j, []tagNameAndUniq{{name: "A", uniq: "a"}}, now)
+	// It could fail for the id allocator or tag creation, so just check the code.
+	if sts.Code() != codes.Internal {
+		t.Fatal(sts)
+	}
+}
+
+func TestUpdateExistingTags(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	tag := c.CreateTag()
+	j := c.Job()
+	defer j.Rollback()
+
+	now := tag.Tag.GetModifiedTime().Add(time.Nanosecond)
+	usage := tag.Tag.UsageCount
+
+	if err := updateExistingTags(j, []*schema.Tag{tag.Tag}, now); err != nil {
+		t.Fatal(err)
+	}
+	if err := j.Commit(); err != nil {
+		t.Fatal(err)
+	}
+
+	tag.Refresh()
+	if tag.Tag.GetModifiedTime() != now {
+		t.Fatal("Modified time not updated")
+	}
+	if tag.Tag.UsageCount != usage+1 {
+		t.Fatal("Usage count not updated")
+	}
+}
+
+func TestUpdateExistingTags_CantPrepare(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	tag := c.CreateTag()
+	j := c.Job()
+	j.Rollback()
+
+	sts := updateExistingTags(j, []*schema.Tag{tag.Tag}, tag.Tag.GetModifiedTime())
+	expected := status.Internal(nil, "can't update tag")
+	compareStatus(t, sts, expected)
+}
+
+func TestFindExistingTagsByName_AllFound(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	tag1 := c.CreateTag()
+	tag2 := c.CreateTag()
+	// create another random tag, but we won't use it.
+	c.CreateTag()
+
+	j := c.Job()
+	defer j.Rollback()
+
+	existing := []tagNameAndUniq{
+		{name: tag2.Tag.Name, uniq: schema.TagUniqueName(tag2.Tag.Name)},
+		{name: tag1.Tag.Name, uniq: schema.TagUniqueName(tag1.Tag.Name)},
+	}
+
+	tags, unknown, err := findExistingTagsByName(j, existing)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Take advantage of the fact that findExistingTagsByName returns tags in order.
+	// This will have to change eventually.
+	if len(tags) != 2 || tags[0].TagId != tag2.Tag.TagId || tags[1].TagId != tag1.Tag.TagId {
+		t.Fatal("Tags mismatch", tags, tag1, tag2)
+	}
+	if len(unknown) != 0 {
+		t.Fatal("All tags should have been found", unknown)
+	}
+}
+
+func TestFindExistingTagsByName_SomeFound(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	tag1 := c.CreateTag()
+	// create another random tag, but we won't use it.
+	c.CreateTag()
+
+	j := c.Job()
+	defer j.Rollback()
+
+	existing := []tagNameAndUniq{
+		{name: "Missing", uniq: schema.TagUniqueName("Missing")},
+		{name: tag1.Tag.Name, uniq: schema.TagUniqueName(tag1.Tag.Name)},
+	}
+
+	tags, unknown, err := findExistingTagsByName(j, existing)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tags) != 1 || tags[0].TagId != tag1.Tag.TagId {
+		t.Fatal("Tags mismatch", tags, *tag1.Tag)
+	}
+	if len(unknown) != 1 || unknown[0] != existing[0] {
+		t.Fatal("Unknown tag should have been found", unknown)
+	}
+}
+
+func TestFindExistingTagsByName_NoneFound(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	// create a random tag, but we won't use it.
+	c.CreateTag()
+
+	j := c.Job()
+	defer j.Rollback()
+
+	existing := []tagNameAndUniq{
+		{name: "Missing", uniq: schema.TagUniqueName("Missing")},
+		{name: "othertag", uniq: schema.TagUniqueName("othertag")},
+	}
+
+	tags, unknown, err := findExistingTagsByName(j, existing)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tags) != 0 {
+		t.Fatal("No tags should be found", tags)
+	}
+	// Again take advantage of deterministic ordering.
+	if len(unknown) != 2 || unknown[0] != existing[0] || unknown[1] != existing[1] {
+		t.Fatal("Unknown tag should have been found", unknown)
+	}
+}
+
+func TestFindUnattachedTagNames_AllNew(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+	tags := []*schema.Tag{c.CreateTag().Tag, c.CreateTag().Tag}
+
+	newnames := []tagNameAndUniq{
+		{name: "Missing", uniq: schema.TagUniqueName("Missing")},
+	}
+
+	names := findUnattachedTagNames(tags, newnames)
+	if len(names) != 1 || names[0] != newnames[0] {
+		t.Fatal("Names should have been found", names)
+	}
+}
+
+func TestFindUnattachedTagNames_SomeNew(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+	tags := []*schema.Tag{c.CreateTag().Tag, c.CreateTag().Tag}
+
+	newnames := []tagNameAndUniq{
+		{name: "Missing", uniq: schema.TagUniqueName("Missing")},
+		{name: tags[0].Name, uniq: schema.TagUniqueName(tags[0].Name)},
+	}
+
+	names := findUnattachedTagNames(tags, newnames)
+	if len(names) != 1 || names[0] != newnames[0] {
+		t.Fatal("Names should have been found", names)
+	}
+}
+
+func TestFindUnattachedTagNames_NoneNew(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+	tags := []*schema.Tag{c.CreateTag().Tag, c.CreateTag().Tag}
+
+	newnames := []tagNameAndUniq{
+		{name: tags[1].Name, uniq: schema.TagUniqueName(tags[1].Name)},
+		{name: tags[0].Name, uniq: schema.TagUniqueName(tags[0].Name)},
+	}
+
+	names := findUnattachedTagNames(tags, newnames)
+	if len(names) != 0 {
+		t.Fatal("Names shouldn't have been found", names)
+	}
+}
+
+func TestFindAttachedPicTags_CantPrepare(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	j := c.Job()
+	j.Rollback()
+
+	_, _, sts := findAttachedPicTags(j, 0)
+	expected := status.Internal(nil, "cant't find pic tags")
+	compareStatus(t, sts, expected)
+}
+
+func TestFindAttachedPicTags_NoTags(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	p := c.CreatePic()
+
+	j := c.Job()
+	defer j.Rollback()
+
+	tags, picTags, err := findAttachedPicTags(j, p.Pic.PicId)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tags) != 0 || len(picTags) != 0 {
+		t.Fatal("Shouldn't have found any tags", tags, picTags)
+	}
+}
+
+func TestFindAttachedPicTags(t *testing.T) {
+	c := Container(t)
+	defer c.Close()
+
+	p := c.CreatePic()
+	tag := c.CreateTag()
+	picTag := c.CreatePicTag(p, tag)
+
+	j := c.Job()
+	defer j.Rollback()
+
+	tags, picTags, err := findAttachedPicTags(j, p.Pic.PicId)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tags) != 1 || len(picTags) != 1 {
+		t.Fatal("Wrong tags", tags, picTags)
+	}
+	if !proto.Equal(tags[0], tag.Tag) || !proto.Equal(picTags[0], picTag.PicTag) {
+		t.Fatal("Tags mismatch", tags, picTags)
+	}
+}
