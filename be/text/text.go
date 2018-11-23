4fe643278de477757bc9abac634db7ceaa154054
Carl Mastrangelo <carl@carlmastrangelo.com>
2018-11-23 13:57:08 -0800

    be/text: add validation functions

--- be/text/text.go
+++ be/text/text.go
@@ -2,6 +2,7 @@ package text
 
 import (
 	"unicode"
+	"unicode/utf8"
 
 	"golang.org/x/text/unicode/rangetable"
 
@@ -42,24 +43,51 @@ var notAllowedRange *unicode.RangeTable
 
 func init() {
 	_, ncc := buildCc()
-
 	notAllowedRange = rangetable.Merge(ncc, unicode.Co, unicode.Cs, unicode.Noncharacter_Code_Point)
-	// Forbidden:
-	// unicode.Noncharacter_Code_Point
-
 }
 
-func ValidateCharset(s string) error {
-	return validateCharset(s)
+// ValidateEncoding ensures that the given string is valid UTF-8.
+func ValidateEncoding(s string) error {
+	return validateEncoding(s)
 }
 
-func validateCharset(s string) status.S {
+func validateEncoding(s string) status.S {
+	if !utf8.ValidString(s) {
+		return status.InvalidArgument(nil, "bad utf8 encoding")
+	}
 	return nil
 }
 
-/*
-#      Lu + Ll + Lt + Lm + Lo + Nl
-#    + Other_ID_Start
-#    - Pattern_Syntax
-#    - Pattern_White_Space
-*/
+// ValidateCodepoints ensures that the string only contains reasonable characters.  By default,
+// Unicode classes Cc (controls), Co (private use), Cs (surrogates), and Noncharacters are
+// forbidden.  (\t, \r, and \n are allowed, though).   Allowed classes include L (letters), M
+// (marks), N (numbers), P (punctuation), S (symbols), Z (separators/spaces), and Cf (formats).
+// Additionally, reserved characters, i.e. those for future use are allowed, as they may become
+// mapped in the future.
+func ValidateCodepoints(s string) error {
+	return validateCodepoints(s)
+}
+
+// validateCodepoints does a blacklist check rather than a whitelist.  Since the allowed and not
+// allowed sets are finite and exclusive, it is equivalent to a whitelist check.  It's possible
+// that the unicode spec could move some characters from the allowed range into the not allowed
+// range (i.e. from reserved to control), but that's less bad than rejecting future valid sequences
+// (i.e. reserved to letter).
+func validateCodepoints(s string) status.S {
+	for i, r := range s {
+		// Incase s hasn't been checked with validateEncoding, RuneError can slip in.  Actual RuneError
+		// is okay, because the user who provided the text may only have the corrupted text to begin
+		// with, and may provide U+FFFD.  To distinguish between a real U+FFFD, and a corrupted created
+		// by Go, check the length of it.  In case it's bad, return an Internal rather than
+		/// InvalidArgument.
+		if r == utf8.RuneError {
+			if _, ln := utf8.DecodeRuneInString(s[i:]); ln <= 1 {
+				return status.Internalf(nil, "bad utf8 encoding at pos %d", i)
+			}
+		}
+		if unicode.Is(notAllowedRange, r) {
+			return status.InvalidArgumentf(nil, "unsupported code point %U at pos %d", r, i)
+		}
+	}
+	return nil
+}
--- be/text/text_test.go
+++ be/text/text_test.go
@@ -1,16 +1,18 @@
 package text
 
 import (
+	"strings"
 	"testing"
 	"unicode"
 
 	"golang.org/x/text/unicode/rangetable"
+	"google.golang.org/grpc/codes"
 )
 
 // TODO: check this for casefold equiv
 //0041+0301+0328 = 0041+0328+0301
 
-func TestAllowedChars(t *testing.T) {
+func TestAllowedCodePointsExclusive(t *testing.T) {
 	allowedcc, _ := buildCc()
 	allowed := rangetable.Merge(
 		unicode.Letter,
@@ -36,5 +38,89 @@ func TestAllowedChars(t *testing.T) {
 	// Unicode 10.0 says it's this much
 	if have, want := reserved, 837775; have != want {
 		t.Error("have", have, "want", want)
+	}
+}
+
+func TestValidateEncoding(t *testing.T) {
+	valid := []string{
+		"\u0000",                               // null
+		"foobar",                               // regular
+		"\uFFFD",                               // replacement char
+		string([]byte{0xF3, 0xBF, 0xBF, 0xBF}), // max codepoint
+		"\uF8FF",                               // private use
+	}
+	invalid := []string{
+		string([]byte{0xC0, 0x00}),
+		string([]byte{0xC2}),
+		string([]byte{0xF7, 0xBF, 0xBF, 0xBF}), // over max codepoint
+		string([]byte{0xED, 0xa0, 0x80}),       // surrogate
+	}
+	for _, s := range valid {
+		sts := validateEncoding(s)
+		if sts != nil {
+			t.Error("failed on", s, sts)
+		}
+	}
+	for _, s := range invalid {
+		sts := validateEncoding(s)
+		if sts == nil {
+			t.Fatal("expected error", s)
+		}
+		if have, want := sts.Code(), codes.InvalidArgument; have != want {
+			t.Error("have", have, "want", want, "for", s)
+		}
+		if have, want := sts.Message(), "bad utf8"; !strings.Contains(have, want) {
+			t.Error("have", have, "want", want, "for", s)
+		}
+	}
+}
+
+func TestValidateCodepoints(t *testing.T) {
+	valid := []string{
+		"\t\r\n",                                 // special control
+		"foobar\U000E0100",                       // mark
+		"  123!@#.  ",                            // number, symbol, punct, space
+		"\U0001F441\uFE0F\u200D\U0001F5E8\uFE0F", // emoji, format
+		"\uFFFD\uFFFC",                           // replacement chars
+		"\U000A0000",                             // Reserved (as of Unicode 11)
+	}
+	invalid := []string{
+		"\u0000",                               // null (control)
+		"\uF8FF",                               // private use
+		string([]byte{0xF3, 0xBF, 0xBF, 0xBF}), // max codepoint (non character)
+	}
+	badutf8 := []string{
+		string([]byte{0xED, 0xa0, 0x80}),       // surrogate
+		string([]byte{0xF7, 0xBF, 0xBF, 0xBF}), // over max codepoint
+	}
+	for _, s := range valid {
+		sts := validateCodepoints(s)
+		if sts != nil {
+			t.Error("failed on", s, sts)
+		}
+	}
+	for i, s := range invalid {
+		sts := validateCodepoints(s)
+		if sts == nil {
+			t.Fatal("expected error", i, s)
+		}
+		if have, want := sts.Code(), codes.InvalidArgument; have != want {
+			t.Error("have", have, "want", want, "for", i, s)
+		}
+		if have, want := sts.Message(), "unsupported code point"; !strings.Contains(have, want) {
+			t.Error("have", have, "want", want, "for", i, s)
+		}
+	}
+	for i, s := range badutf8 {
+		sts := validateCodepoints(s)
+		if sts == nil {
+			t.Fatal("expected error", i, s)
+		}
+		if have, want := sts.Code(), codes.Internal; have != want {
+			t.Error("have", have, "want", want, "for", i, s)
+		}
+		if have, want := sts.Message(), "bad utf8 encoding"; !strings.Contains(have, want) {
+			t.Error("have", have, "want", want, "for", i, s)
+		}
 	}
 }
